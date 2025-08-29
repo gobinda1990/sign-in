e09d91a9a0977649fa82d6d11744cd2087008864
gobinda1990 <68889122+gobinda1990@users.noreply.github.com>
2025-08-29 15:29:07 +0530

    Update BackendApplication.java

--- backend/src/main/java/comtax/gov/webapp/BackendApplication.java
+++ backend/src/main/java/comtax/gov/webapp/BackendApplication.java
@@ -2,8 +2,9 @@ package comtax.gov.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
+@ComponentScan(basePackages = "comtax.gov")
 public class BackendApplication {
 
 	public static void main(String[] args) {
